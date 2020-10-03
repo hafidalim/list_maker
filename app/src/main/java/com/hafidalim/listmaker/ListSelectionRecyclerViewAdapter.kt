a492229c51c8d810b075a351a3b688dae7611309
hafidam <hafidd456@gmail.com>
2020-10-03 13:16:41 +0700

    create listmaker

---
+++ app/src/main/java/com/hafidalim/listmaker/ListSelectionRecyclerViewAdapter.kt
@@ -0,0 +1,24 @@
+package com.hafidalim.listmaker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListSelectionRecyclerViewAdapter : RecyclerView.Adapter<ListSelectionViewHolder>() {
+    val listTitle = arrayOf("Shopping List", "Chores", "Android Tutorials")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_selection_view_holder, parent, false)
+        return ListSelectionViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listTitle.size
+    }
+
+    override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
+        holder.listPosition.text = (position + 1).toString()
+        holder.listTitle.text = listTitle[position]
+
+    }
+
+}
